32abc5e33ed762e130cce07b7f1140c42d31a8f7
Yusarzainurrohim <113924052+Yusarzainurrohim@users.noreply.github.com>
2022-09-20 14:23:31 +0700

    Create Hello.cpp

---
+++ Hello.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<< "Nama : Yusar Zainur Rohim" <<endl;
+cout<< "Kelas : X PPLG 1"<<endl;
+cout<< "========================="<<endl;
+cout<< ";angka 1 = 10"<<endl;
+cout<< ";angka 2 = 90"<<endl;
+cout<< "hasil:"<<10+90 <<endl;
+return 0;}
